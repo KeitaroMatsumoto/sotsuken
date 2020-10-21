c0855e09f87669176ea98a42256dcff1f3252329
松本圭太郎 <matsumotokeitarou@MacBook-Pro-4.local>
2020-10-21 11:43:44 +0900

    add new file

---
+++ elec_eq.cpp
@@ -0,0 +1,20 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
+# include <iostream>
+# include <fstream>
+# include <time.h>
+using namespace std;
+
+double eq_motion(double *x,double *y,double *z,double *vx,double *vy,double *vz,double mass,double dt){
+
+  
+}
+
+int main(){
+
+  double x,y,z,vx,vy,vz,t;
+  double dt = 0.01,mass=1.,time_max=10000.; //parameters
+  int i;
+
+}
